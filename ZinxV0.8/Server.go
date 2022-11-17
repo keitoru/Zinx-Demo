ff4949f2258d08d72a77bc4d76c4940915087155
zhu.wanghe <zhu.wanghe@byxgame.com>
2022-11-17 16:26:21 +0800

    v0.8

---
+++ ZinxV0.8/Server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"zinx/ziface"
+	"zinx/znet"
+)
+
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+// Handle test
+func (pr *PingRouter) Handle(req ziface.IRequest) {
+	fmt.Println("Call Router Handle...")
+
+	//先读取客户端的数据，再回写ping...ping...ping
+	fmt.Println("recv from client : msgId=", req.GetMsgID(), ", data=", string(req.GetMsgData()))
+
+	//回写数据
+	if err := req.GetConnection().SendMsgData(200, []byte("ping...ping...ping")); err != nil {
+		fmt.Println(err)
+	}
+}
+
+//HelloZinxRouter Handle
+type HelloZinxRouter struct {
+	znet.BaseRouter
+}
+
+func (hzr *HelloZinxRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call HelloZinxRouter Handle")
+	//先读取客户端的数据，再回写ping...ping...ping
+	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetMsgData()))
+
+	err := request.GetConnection().SendMsgData(201, []byte("Hello Zinx Router V0.8"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	// 创建一个server句柄，使用Zinx的api
+	s := znet.NewServer("[zinx V0.7]")
+
+	s.AddRouter(0, &PingRouter{})
+	s.AddRouter(1, &HelloZinxRouter{})
+
+	// 启动server
+	s.Serve()
+}
